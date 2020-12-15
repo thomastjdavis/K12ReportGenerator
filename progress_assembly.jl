b521622a798ac619444a11db04a61b37410d59f9
TJ Davis <thomas.davis@mavs.uta.edu>
2020-12-15 08:48:53 +0800

    Updated warnings.js, added log file functionality.
    Updated warnings.js, added a button to download a .txt file with students that need to raise their grades to a 90%. Wrote a log file

--- progress_assembly.jl
+++ progress_assembly.jl
@@ -3,7 +3,7 @@
 #separate via subject level
 
 #Niceities:
-#export to csv for cool analysis/create a log file
+#export to csv for cool analysis
 
 
 using Dates, Gumbo
@@ -94,26 +94,50 @@ td.right,th.right {
 <script>
 	/*
 	Changes the background color of rows with falling grades to "ff3333"
-	TO DO: add log message with students who are failing
-*/
+		Grades that are between 70% and 90% are shaed to "ffeo55"
+	*/
+	var failing = 'The following students are currently failing:';
+	var improve = 'The following students need to improve their averages:'
 function warnings(){
 	tbls = document.getElementsByClassName("course_table")
 	var len = tbls["length"]
+		
 	var i,j
+		
 	for(i=0; i<len; i++){
 		len1 = tbls[i]["rows"].length
+		show_name = true
 		for(j=1;j<len1;j++){
 			str = tbls[i]["rows"][j]["cells"][3]["textContent"]
 			str2 = tbls[i]["rows"][j]["cells"][4]["textContent"]
 			var perc = parseFloat(str)
 			var perc2 = parseFloat(str2)
 			if(perc<= 70.0){
+				if(show_name){
+					failing += stds[i].innerHTML
+					show_name = false
+				}
+				failing += tbls[i]["rows"][j]["cells"][0]["innerHTML"] 
+				failing += str
 				tbls[i]["rows"][j]["cells"][3]["innerHTML"]=emph(str)
 				for(k=0;k<7;k++){
 					tbls[i]["rows"][j]["cells"][k]["bgColor"]="ff2e2e"
-				}				
+				}
+				continue;
 			}
-			if(perc2<2.0){
+			if(perc< 90.0){
+				if(show_name){
+					improve += stds[i].innerHTML
+					show_name = false
+				}
+				improve += tbls[i]["rows"][j]["cells"][0]["innerHTML"] 
+				improve += str 
+				tbls[i]["rows"][j]["cells"][3]["innerHTML"]=emph(str)
+				for(k=0;k<7;k++){
+					tbls[i]["rows"][j]["cells"][k]["bgColor"]="ffe055"
+				}	
+			}
+			if(perc2<4.0){
 				tbls[i]["rows"][j]["cells"][4]["bgColor"]="55eedd"
 				tbls[i]["rows"][j]["cells"][4]["innerHTML"]= emph(str2)
 			}
@@ -124,21 +148,47 @@ function warnings(){
 function emph(str){
 	return "*"+str+"*"
 }
+		
+
+ let saveFile = () => {
+        
+        // This variable stores all the data.
+        let data = failing+improve
+        
+        // Convert the text to BLOB.
+        const textToBLOB = new Blob([data], { type: 'text/plain' });
+        const sFileName = 'progress_notes.txt';	   // The file to save the data.
+
+        let newLink = document.createElement("a");
+        newLink.download = sFileName;
+
+        if (window.webkitURL != null) {
+            newLink.href = window.webkitURL.createObjectURL(textToBLOB);
+        }
+        else {
+            newLink.href = window.URL.createObjectURL(textToBLOB);
+            newLink.style.display = "none";
+            document.body.appendChild(newLink);
+        }
+
+        newLink.click(); 
+    }		
 </script>
 
 <div id="progress_report"><div align="center"><img src="https://key12.k12.com/i/logo_keystone_clear.png" alt="Keystone Logo"><p class="center">920 Central Road, Bloomsburg, PA 17815 <br>Phone (800) 255-4937<br>Fax (570) 300-2346<br>info@keystonehighschool.com - <a href="http://www.keystonehighschool.com">www.keystonehighschool.com</a><br></p></div>
 
 </head>
 <body onLoad="warnings()">
-""")
+<div>
+	<input type="button" id="bt" value="Click to download notes about the progress reports" onclick="saveFile()"/>
+</div>""")
 end
 
 function createReport(students::Dict{Int64,String},grouping::String)
 	
-	cd("/mnt/c/Users/Cheri/Desktop/workCode/email.reminders/progress_reports/11-24-2020")
 	f = open("$(grouping).html","w")
 	
-	header("11/23/2020",f)
+	header("12/1/2020",f)
 	
 	write(f,"<h1>$(grouping)'s progress</h1>")
 	for value in keys(students)
@@ -163,25 +213,32 @@ end
 function createMaster()
 	#make sure the working directory is correct
 	
-	cd("/mnt/c/Users/Cheri/Desktop/workCode/email.reminders/progress_reports/11-24-2020")
 	f = open("master_progress_current.html","w")
+	report_log = open("master_progress_current.log","w")
 	
 	#adds the logo, info, and style information at the top 
-	header("11/23/2020",f)
+	header("12/8/2020",f)
 	
 	
-	write(f,"<h1>G10's Progress</h1>")
+	print(f,"<h1>G10's Progress</h1>")
+	print(report_log,"...Writing G10's progres...")
 	for value in keys(g10_students)
 		ID = "$value"
 		docname = "keystone_progress_report_$ID"*".html"
 		try
 			file = parsehtml(read(docname,String))
-			println(f,"$ (g10_students[value]):\n")
-			println(f,repr(file.root[2][1][2][2])[21:end])
-			println(f,repr(file.root[2][1][2][4])[21:end])
+			println("testing if $(g10_students[value]) has data available")
+			
+			str1 = repr(file.root[2][1][2][2])[21:end]
+			str2 = repr(file.root[2][1][2][4])[21:end]
+			println("data found!")
+			println(f,"""<h2 align="center">$(g10_students[value]) data:</h2>\n""")
+			println(f,str1)
+			println(f,str2)
 			println(f,"<br><br>")
 		catch e 
 			println(f,"data for $(g10_students[value]) not found <br><br><br>")
+			println(report_log,"juliadata for $(g10_students[value]) not found")
 			continue
 		end
 	end	
@@ -193,18 +250,27 @@ function createMaster()
 		docname = "keystone_progress_report_$ID"*".html"
 		try
 			file = parsehtml(read(docname,String))
-			println(f," $(g11_students[value]):")
-			println(f,repr(file.root[2][1][2][2])[21:end])
-			println(f,repr(file.root[2][1][2][4])[21:end])
+			println("testing if $(g10_students[value]) has data available")
+			
+			str1 = repr(file.root[2][1][2][2])[21:end]
+			str2 = repr(file.root[2][1][2][4])[21:end]
+			println("data found!")
+			println(f,"""<h2 align="center">$(g10_students[value]) data:</h2>\n""")
+			println(f,str1)
+			println(f,str2)
 			println(f,"<br><br>")
 		catch e 
 			write(f,"data for $(g11_students[value]) not found <br><br><br>")
+			println("data for $(g11_students[value]) not found")
+			println(report_log,"juliadata for $(g11_students[value]) not found")
 			continue
 		end
 	end	
 	println(f,"<p>See source code at <b>github.com/thomastjdavis/K12ReportGenerator</b></p>")
 	close(f)
-	println("Master progress report assembled. ")
+	println("\n Master progress report assembled. ")
+	 
+	
 	end
 	
 	
